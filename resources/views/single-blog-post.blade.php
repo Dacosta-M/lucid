2eb73ef2e0c16a5922a9aea3e9fb3b11a8e5557c
JeffOgah <jeff.ogah@gmail.com>
2019-09-05 10:26:07 +0100

    comment fixes & included share icons

--- resources/views/single-blog-post.blade.php
+++ resources/views/single-blog-post.blade.php
@@ -32,23 +32,23 @@ $location= 'singlePost';
 @section('content')
 <!-- Beginning of Post Content -->
 <style>
-.standard-color{
-  background: #871e99;
-  color:#fff;
-  border:1px solid #871e99;
-}
+    .standard-color {
+        background: #871e99;
+        color: #fff;
+        border: 1px solid #871e99;
+    }
 
-.standard-color:hover{
-  background: #871e99 !important;
-  color:#fff;
-  border:1px solid #871e99 !important;
-}
+    .standard-color:hover {
+        background: #871e99 !important;
+        color: #fff;
+        border: 1px solid #871e99 !important;
+    }
 
-.text-danger{
-  font-weight:400px !important;
-  font-size:12px !important;
+    .text-danger {
+        font-weight: 400px !important;
+        font-size: 12px !important;
 
-}
+    }
 </style>
 <div class="post-content">
     <div class="post-content-body m-0">
@@ -67,23 +67,25 @@ $location= 'singlePost';
         </div>
     </div>
 </div>
+<div class="d-flex">
+    <div class="mt-2 mr-3">
+        <a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-show-count="false"><i class="icon ion-logo-twitter h2 mx-2"></i></a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+    </div>
+
+    <div class="mt-2 mr-3">
+        <iframe src="https://www.facebook.com/plugins/share_button.php?href=https%3A%2F%2Flucid.blog&layout=button&size=small&appId=173297093603387&width=59&height=20" width="59" height="20" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
+    </div>
+
+    <div class="mt-1 mr-3">
+        <script src="https://platform.linkedin.com/in.js" type="text/javascript">
+            lang: en_US
+        </script>
+        <script type="IN/Share" data-url="https://www.linkedin.com"></script>
+    </div>
+</div>
 <hr style="padding-bottom:20px">
 <div class="">
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <!-- convert to markdown script ends -->
-  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <h6 class="font-weight-bold">Comments: </h6>
-  <div class="comments">
-
-    <div class="" style="text-align: -webkit-center">
-     <div class="spinner" style="    position: inherit;"></div></div>
-</div>
-</div>
-<br><br>
-<hr style="padding-top:20px">
-<div class="">
-    <p>Write a comment</p>
     <form method="post" action="" autocomplete="off" enctype="multipart/form-data" class="mb-3 commentForm">
         @csrf
         <div class="form-group">
@@ -96,110 +98,125 @@ $location= 'singlePost';
 
         </div>
         <div class="text-right">
-        @auth
+            @auth
             <button type="submit" name="comment" class="btn bg-alt text-white">Comment</button>
-        @endauth
-        @guest
-         <button type="button" name="loginpopup" id="loginpopup" class="btn bg-alt text-white">Comment</button>
-        @endguest
+            @endauth
+            @guest
+            <button type="button" name="loginpopup" id="loginpopup" class="btn bg-alt text-white">Comment</button>
+            @endguest
         </div>
     </form>
+</div>
+<hr style="padding-top:20px">
+<div class="">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <!-- convert to markdown script ends -->
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <h6 class="font-weight-bold">Comments: </h6>
+    <div class="comments">
+
+        <div class="" style="text-align: -webkit-center">
+            <div class="spinner" style="    position: inherit;"></div>
+        </div>
+    </div>
 </div>
 
 <!-- End of Post Content -->
 <script>
-const k= jQuery.noConflict();
-function reply(c_id,user_id,username) {
-                        k("#parents_id").attr("value",c_id);
-                        k("#user_id").attr("value",user_id);
-                        k("#type").attr("value","Replied");
-                        k("#body").focus();
-                        k("#body").attr("placeholder","Replying to @"+username);
+    const k = jQuery.noConflict();
 
-}
+    function reply(c_id, user_id, username) {
+        k("#parents_id").attr("value", c_id);
+        k("#user_id").attr("value", user_id);
+        k("#type").attr("value", "Replied");
+        k("#body").focus();
+        k("#body").attr("placeholder", "Replying to @" + username);
+
+    }
 </script>
 <script>
-const j = jQuery.noConflict();
- j(document).ready(function (){
-   function getComment() {
+    const j = jQuery.noConflict();
+    j(document).ready(function() {
+        function getComment() {
 
-    const route = "{{ secure_url($user->username.'/comments',['post_id'=>$post['id']])  }}"
-    j.ajaxSetup({
-        headers:{
-            'X-CSRF-TOKEN': j('meta[name="csrf-token"]').attr('content')
-        }
-     })
-     setTimeout(() => {
-     j.ajax({
-         type:'GET',
-         url:route,
-         contentType:false,
-         processData:false,
-         success:function (data){
-             j('.comments').html(data);
-         }
-     })
-
-     }, 5000);
-   }
-     // initial call, or just call refresh directly
-     // initial call, or just call refresh directly
-     setTimeout(getComment(), 5000);
-    const commentForm = document.querySelector('.commentForm');
-    const commentBtn  = document.querySelector('button[name="comment"]');
-    if(commentBtn !=null){
-        commentForm.onsubmit = commentBtn.addEventListener('click',function(e){
-            e.preventDefault();
-
-            const formData = new FormData(commentForm);
-            const saveComment = "{{ secure_url($user->username.'/save-comment')  }}";
-            if(formData.get('body') == "") {
-                j('.text-danger').show();
-            }else{
-                j('.text-danger').hide();
+            const route = "{{ secure_url($user->username.'/comments',['post_id'=>$post['id']])  }}"
+            j.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': j('meta[name="csrf-token"]').attr('content')
+                }
+            })
+            setTimeout(() => {
                 j.ajax({
-                    type:"POST",
-                    url:saveComment,
-                    dataType:'json',
-                    data:formData,
-                    contentType:false,
-                    processData:false,
-                    beforeSend:function(){
-                        commentBtn.setAttribute('disabled','disabled');
-                    },
-                    success:function(response){
-                       // console.log(response);
-                        commentBtn.removeAttribute('disabled');
-                        commentForm.reset();
-                        getComment();
+                    type: 'GET',
+                    url: route,
+                    contentType: false,
+                    processData: false,
+                    success: function(data) {
+                        j('.comments').html(data);
                     }
                 })
-            }
 
+            }, 5000);
+        }
+        // initial call, or just call refresh directly
+        // initial call, or just call refresh directly
+        setTimeout(getComment(), 5000);
+        const commentForm = document.querySelector('.commentForm');
+        const commentBtn = document.querySelector('button[name="comment"]');
+        if (commentBtn != null) {
+            commentForm.onsubmit = commentBtn.addEventListener('click', function(e) {
+                e.preventDefault();
+
+                const formData = new FormData(commentForm);
+                const saveComment = "{{ secure_url($user->username.'/save-comment')  }}";
+                if (formData.get('body') == "") {
+                    j('.text-danger').show();
+                } else {
+                    j('.text-danger').hide();
+                    j.ajax({
+                        type: "POST",
+                        url: saveComment,
+                        dataType: 'json',
+                        data: formData,
+                        contentType: false,
+                        processData: false,
+                        beforeSend: function() {
+                            commentBtn.setAttribute('disabled', 'disabled');
+                        },
+                        success: function(response) {
+                            // console.log(response);
+                            commentBtn.removeAttribute('disabled');
+                            commentForm.reset();
+                            getComment();
+                        }
+                    })
+                }
+
+            })
+        }
+
+
+        j('#loginpopup').on('click', function() {
+            swal({
+                text: 'Opps! Login to comment on this post',
+                icon: "info",
+                button: {
+                    text: "Login",
+                    value: true,
+                    visible: true,
+                    className: "standard-color",
+                    closeModal: true,
+                },
+            });
+
+            j('.standard-color').on('click', function() {
+                window.location = "/login";
+            })
         })
-    }
 
 
-    j('#loginpopup').on('click',function(){
-        swal({
-            text: 'Opps! Login to comment on this post',
-            icon: "info",
-            button: {
-            text: "Login",
-            value: true,
-            visible: true,
-            className: "standard-color",
-            closeModal: true,
-            },
-        });
-
-        j('.standard-color').on('click',function(){
-            window.location = "/login";
-        })
     })
-
-
- })
 </script>
 
 @endsection
